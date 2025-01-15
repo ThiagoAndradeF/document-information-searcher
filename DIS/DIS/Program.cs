707d5271117407edf0e7ff75edcc3643b5d73fee
Thiago Andrade Ferreira <thiagoandrade160302@gmail.com>
2025-01-15 16:45:49 -0300

    Add Qdrant VDB
    Remove Milvus and Integrations Qdrant

--- DIS/DIS/Program.cs
+++ DIS/DIS/Program.cs
@@ -1,26 +1,29 @@
 using DIS.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Milvus.Client;
 using OpenAI;
 using OpenAI.Managers;
+using Qdrant.Client;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var openaiKey = builder.Configuration["OpenAiKey"];
+var qdrantKey = builder.Configuration["QdrantKey"]; 
+var qdrantHost = builder.Configuration["QdrantHost"];
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var milvusHost = builder.Configuration["Milvus:Host"];
-var milvusPort = int.Parse(builder.Configuration["Milvus:Port"]);
-
 builder.Services.AddSingleton(new OpenAIService(new OpenAiOptions()
 {
-	ApiKey = "api_key"
+	ApiKey = openaiKey
 }));
-builder.Services.AddSingleton<MilvusClient>(provider => new MilvusClient(milvusHost, milvusPort));
+
+builder.Services.AddSingleton(new QdrantClient(
+  host: qdrantHost,
+  https: true,
+  apiKey: qdrantKey
+));
+
 builder.Services.AddScoped<IMilvusClientService, MilvusClientService>();
 builder.Services.AddScoped<ITextAnalysisService, TextAnalysisService>();
-
 builder.Services.AddScoped<DocumentService>();
 
 
